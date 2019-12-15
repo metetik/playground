bb77d1f41fc4d2ecd23e026c6fdd988c25b4d297
metetik <m.e.tetik03@gmail.com>
2019-12-15 23:04:09 +0300

    register sayfası oluşturuldu

--- Python/flask3/flask_deneme1.py
+++ Python/flask3/flask_deneme1.py
@@ -1,13 +1,29 @@
 #flask - mysql
 
-from flask import Flask,render_template,flash,url_for,session,logging,request
-#from flask_mysqldb import MySQL
+from flask import Flask,render_template,flash,redirect,url_for,session,logging,request
+from flask_mysqldb import MySQL
 from wtforms import Form,StringField,TextAreaField,PasswordField,validators
 from passlib.hash import sha256_crypt
 
 #Kullanıcı kayıt formu
 class RegisterForm(Form):
-    name = StringField()
+    #isim soyisim alanı oluştur,min 4 max 25 değer alabilsin
+    name = StringField("İsim Soyisim",validators = [validators.Length(min = 4,max = 25)])
+    
+    username = StringField("Kullanıcı Adı",validators = [validators.Length(min = 5,max = 35)])
+    
+    #girilen adres email mi kontrol et, değilse mesajı yaz
+    email = StringField("E-Mail",validators = [validators.Email(message = "Lütfen geçerli bir email adresi giriniz.")])
+    
+    password = PasswordField("Parola",validators = [
+        validators.DataRequired(message = "Lütfen bir parola giriniz"),
+        
+        #parola ile confirm uyuşmuyorsa mesajı yazdır
+        validators.EqualTo(fieldname = "confirm", message = "Parola uyuşmuyor..") 
+    ])
+
+    confirm = PasswordField("Parola Doğrula")
+
 app = Flask("__name__")
 
 # Veritabanı ayarları
@@ -27,8 +43,16 @@ def index():
 def about():
     return "Hakkımda"
 
-#dinamik url
-@app.route("/dinamik/<string:id>")
+#kayıt olma
+@app.route("/register",methods = ["GET","POST"])#Hem get request hem de post request alabilir
+def register():
+    form = RegisterForm(request.form)
+
+    if request.method == "POST":
+        return redirect(url_for("index"))
+    else:
+        return render_template("register.html",form = form) 
+     
 def dinamik(id):
     return "dinamik url id : " + id
 if __name__ == "__main__":
